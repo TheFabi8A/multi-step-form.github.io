3979d7ff92701e16859c7c98799b3efefd041494
TheFabi8A <cafaruoc2000@hotmail.com>
2023-04-25 03:12:46 -0500

    day 2

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('test');
+const inputRangePlan = document.getElementById('input-range-plan');
+let monthFreeText = document.querySelectorAll('.month-free-text');
+
+inputRangePlan.addEventListener('change', () => {
+    function seleccionarPlan(value) {
+        if (value === '1') {
+            monthFreeText.forEach((text) => {
+                text.classList.add('hidden');
+            });
+            console.log('monthly');
+        } else if (value === '2') {
+            monthFreeText.forEach((text) => {
+                text.classList.remove('hidden');
+            });
+            console.log('yearly');
+        }
+    }
+    seleccionarPlan(inputRangePlan.value);
+});
